ea61c1ca2614bf6c6ff4179b4d947d839bcae937
Josue Fernandez <josue.0795@gmail.com>
2023-03-17 13:22:25 -0600

    Fixed jest and ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transformIgnorePatterns: ['/node_modules'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 }
